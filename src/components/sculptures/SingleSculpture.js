25d85b7c278262a6dbdce3fa626fe4a2a4644231
kfiddle <kenjfiddle@gmail.com>
2021-06-08 18:15:48 -0400

    hovering effect on tiles

--- src/components/sculptures/SingleSculpture.js
+++ src/components/sculptures/SingleSculpture.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
+
 import styles from "./Sculpture.module.css";
 
 const SingleSculpture = (props) => {
+  const [imageHoverState, setImageHoverState] = useState(false);
+  const imageClass = styles.sculptureImage + '.hover';
+
   const { source, size } = props;
 
   const imageSrc = "images/sculptures/" + source;
 
+  const onHoverHandler = () => {
+    setImageHoverState(true);
+  };
+
+  const hoverOutHandler = () => {
+    setImageHoverState(false);
+  }
+
   return (
-    <div className={`${styles.tileDiv} ${styles[size]}`}>
-      <img src={imageSrc} className={styles.sculptureImage} />
+    <div
+      className={`${styles.tileDiv} ${styles[size]}`}
+      onMouseEnter={onHoverHandler}
+      onMouseLeave={hoverOutHandler}
+    >
+      <img
+        src={imageSrc}
+        className={`${styles.sculptureImage} ${imageHoverState ? styles.hover : ''}`} 
+      />
     </div>
   );
 };
 
 export default SingleSculpture;
+
+// ${imageHoverState ? ".hover" : ""
