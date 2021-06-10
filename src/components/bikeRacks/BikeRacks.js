caac9c99e6637165c2ca9461a62bb1dfa8c2ae67
kfiddle <kenjfiddle@gmail.com>
2021-06-09 20:57:49 -0400

    unmount bike at end

--- src/components/bikeRacks/BikeRacks.js
+++ src/components/bikeRacks/BikeRacks.js
@@ -1,11 +1,18 @@
+import { Fragment, useState } from 'react';
 
 import styles from './BikeRacks.module.css';
 import PennyFarthing from './PennyFarthing';
 
 const BikeRacks = props => {
+    const [bikeRiding, setBikeRiding] = useState(true);
 
+    setTimeout(()=> { setBikeRiding(false)}, 30000);
 
-    return <PennyFarthing />
+    return (
+        <Fragment>
+            {bikeRiding && <PennyFarthing />}
+        </Fragment>
+    ) 
 
 
 };
