b1a867bba2b35c6c6ee7073d429e1941a0c1da93
First Last <samoviskik@gmail.com>
2021-09-21 17:12:41 -0400

    styling

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -35,23 +35,23 @@ Hamilton, showcase a variety of homes and buildings.</p>
 </div>
 
 <div className={styles.accordion}>
-        <p className={styles.missionAndBoardText}>Our Mission</p>
+        <p className={styles.missionAndBoardText}>Our Partners in the Arts</p>
       </div>
 
       <div className={styles.accordionContent}>
-            <p>Jason Elvers,Vorys, Sater, Seymour & Pease, LLP, Columbus, Ohio </p>
-            <p>  Julie Carpenter,Cardinal Heatlh, Dublin, Ohio </p>
-            <p>  Kayleigh Hunnicutt,Dublin City Schools, School Psychologist </p>
-            <p>  Lisa Chapman,Elevated Marketing Services, Columbus, Ohio </p>
-            <p>  Cody Yarletts,Parker Hannifin,Columbus, Ohio </p>
-            <p>  Ryan J. Vonderbrink,Seegrid, New Albany, Ohio </p>
-            <p>  Joshua Yakunich, Ohio State University Wexner Medical Center, Columbus, Ohio </p>
-
-            <p>  Interested in serving on our Board of Directors or volunteering for Edheads?
-                Please check out our listings on VolunteerMatch or contact us at info@edheads.org.
-                For all questions, comments, and concerns, please send an email to info@edheads.org.
-
+            <p>A multi-arts facility given as a gift to the city by
+the community in honor of Hamilton’s bicentennial,
+the Fitton Center features rotating galleries year-round
+that are free to view as well as a plethora of special
+programming ranging from art classes to nationally
+touring theater productions. 
             </p>
+            <hr/>
+            <p>A 265 acre sculpture park and outdoor museum nestled on the edge of town that combines the lure of nature with the dynamic presence of more than 60 pieces of monumental sculpture. One of the few such facilities
+of its scale in the United States, Pyramid Hill is a
+setting where landscape and art come together
+in natural galleries.</p>
+
         </div>
 
       <div className={styles.accordion}>
