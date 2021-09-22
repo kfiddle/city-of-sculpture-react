22cf824bf753f9e200d2273ae8ac4ee22b2cc386
kfiddle <kenjfiddle@gmail.com>
2021-09-22 14:05:06 -0400

    bike rack titles

--- src/components/bikeRacks/BikeRacks.js
+++ src/components/bikeRacks/BikeRacks.js
@@ -9,14 +9,12 @@ import styles from "./BikeRacks.module.css";
 const BikeRacks = (props) => {
   // const [bikeRiding, setBikeRiding] = useState(true);
 
-  const [bikesDivPosition, setBikesDivPosition] = 
-  useState("translateX(100vw)");
+  const [bikesDivPosition, setBikesDivPosition] = useState("translateX(100vw)");
 
   useEffect(() => {
     const slideTimer = setTimeout(() => {
       // setBikesDivPosition("translateX(23vw)");
       setBikesDivPosition("translateX(0)");
-
     }, 500);
 
     // setTimeout(() => {
@@ -29,31 +27,47 @@ const BikeRacks = (props) => {
     V = "vertical",
     HUGE = "huge";
 
-  const listOfBikeRacks = {
-    barclays: H,
-    butlerTech: V,
-    communityFirst: BIG,
-    developmentDisabilities: H,
-    fortHamHospital: V,
-    hamClass: BIG,
-    hamClass25: BIG,
-    hamClass25Owl: BIG,
-    hamiltonParksConservancy: HUGE,
-    hamOrthopaedics: BIG,
-    miamiURegionals: H,
-    pikeFamily: V,
-    primaryHealth: H,
-    thyssenkruppBilsten: V,
-  };
+  // const listOfBikeRacks = {
+  //   barclays: H,
+  //   butlerTech: V,
+  //   communityFirst: BIG,
+  //   developmentDisabilities: H,
+  //   fortHamHospital: V,
+  //   hamClass: BIG,
+  //   hamClass25: BIG,
+  //   hamClass25Owl: BIG,
+  //   hamiltonParksConservancy: HUGE,
+  //   hamOrthopaedics: BIG,
+  //   miamiURegionals: H,
+  //   pikeFamily: V,
+  //   primaryHealth: H,
+  //   thyssenkruppBilsten: V,
+  // };
+
+  const listOfBikeRacks = [
+    { id: 1, title: "some title", source: 'barclays.jpeg' },
+    { id: 2, title: "some title", source: 'butlerTech.jpeg' },
+    { id: 3, title: "some title", source: 'communityFirst.jpeg' },
+    { id: 4, title: "some title", source: 'developmentDisabilities.jpeg' },
+    { id: 5, title: "some title", source: 'fortHamHospital.jpeg' },
+    { id: 6, title: "some title", source: 'hamClass.jpeg' },
+    { id: 7, title: "some title", source: 'hamClass25.jpeg' },
+    { id: 8, title: "some title", source: 'hamClass25Owl.jpeg' },
+    { id: 9, title: "some title", source: 'hamiltonParksConservancy.jpeg' },
+    { id: 10, title: "some title", source: 'miamiURegionals.jpeg' },
+    { id: 11, title: "some title", source: 'pikeFamily.jpeg' },
+    { id: 11, title: "some title", source: 'primaryHealth.jpeg' },
+    { id: 11, title: "some title", source: 'thyssenkruppBilsten.jpeg' },
+  ];
 
   const displayedBikeRacks = [];
 
-  for (let bikeRack in listOfBikeRacks) {
+  for (let bikeRack of listOfBikeRacks) {
     displayedBikeRacks.push(
       <SingleSculpture
-        key={Math.random()}
-        source={bikeRack + ".jpeg"}
-        size={listOfBikeRacks[bikeRack]}
+        key={bikeRack.id}
+        source={bikeRack.source}
+        // size={listOfBikeRacks[bikeRack]}
         title={bikeRack.title}
       ></SingleSculpture>
     );
@@ -61,7 +75,10 @@ const BikeRacks = (props) => {
   console.log(displayedBikeRacks);
 
   return (
-    <div className={styles.bikeAndTiles} style={{transform : bikesDivPosition}}>
+    <div
+      className={styles.bikeAndTiles}
+      style={{ transform: bikesDivPosition }}
+    >
       <TileGallery className={"bikeGalleryDiv"}>
         {displayedBikeRacks}
       </TileGallery>
