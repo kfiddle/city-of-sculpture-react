601d901917e1cb205b89849897f69d3af55646c4
First Last <samoviskik@gmail.com>
2021-09-30 12:05:10 -0400

    trying to figure out contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,14 +5,49 @@ import styles from './Contact.module.css';
 
 const Contact = props => {
 
-    return <SlidingBox>Hello there I am a box. I will soon
-        be filled with lots of cool things.
-        Within me, you will be able to send emails to the 
-        powers that be
-    </SlidingBox>
+    return (
+        <SlidingBox>
+        <div className={styles.innerContentContainer}>
+        
+        <form method="post">
 
+                            <div className={styles.accountRowOne}>
+                                <div className={styles.firstName}>
+                                    <input type="text" placeholder="First Name"
+                                           className={styles.formControl}
+                                           required minlength="1" maxlength="20" />
+                                </div>
 
+                                <div className={styles.lastName}>
+                                    <input type="text" placeholder="Last Name" 
+                                          className={styles.formControl}
+                                           required minlength="1" maxlength="20" />
+                                </div>
+                            </div>
 
+                            <div className={styles.accountRowTwo}>
+                                <div className={styles.email}>
+                                        <input type="email" placeholder="Email" 
+                                               className={styles.formControl} required />
+                                </div>
+                                </div>
+
+                      <div className={styles.description}>
+                                    <input type="text area" name="description" className={styles.formControl} />
+                                </div>
+                          
+
+                            <div className={styles.buttonContainer}>
+                                <button type="submit" className={styles.signUpButton}>Sign Up</button>
+                        
+                            </div>
+                         
+
+                    </form>
+    
+        </div>
+      </SlidingBox>
+      );
 };
 
 export default Contact;
