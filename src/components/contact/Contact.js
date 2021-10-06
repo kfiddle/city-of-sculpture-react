f571e49d3c5f321da49d9795939c822f9e6b86f4
kfiddle <kenjfiddle@gmail.com>
2021-10-06 17:45:26 -0400

    email stuff

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,58 +1,93 @@
+import { useRef, useState } from "react";
 
 import SlidingBox from "../slidingBox/SlidingBox";
 
-import styles from './Contact.module.css';
+import emailjs from "emailjs-com";
 
-import React, { useState } from "react";
+import styles from "./Contact.module.css";
+
+// const userId = "user_ziX5oSLNJRahUxs9dz2xC";
+// const serviceId = "service_whc7i1l";
+// const templateId = "template_xhux42i";
+
 
 const ContactForm = () => {
   const [status, setStatus] = useState("Submit");
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
+
     let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      message: messageRef.current.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
+
+    //   emailjs.send(serviceId, templateId, details, userId);
+
+    
+
+    // setStatus("Sending...");
+    // const { name, email, message } = e.target.elements;
+    // let details = {
+    //   name: name.value,
+    //   email: email.value,
+    //   message: message.value,
+    // };
+    // let response = await fetch("http://localhost:5000/contact", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify(details),
+    // });
+    // setStatus("Submit");
+    // let result = await response.json();
+    // alert(result.status);
   };
-    
-    
-    return (
-        <SlidingBox>
-        <div className={styles.innerContentContainer}>
 
+  return (
+    <SlidingBox>
+      <div className={styles.innerContentContainer}>
         <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input ref={nameRef} type="text" id="name" required />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input ref={emailRef} type="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea ref={messageRef} id="message" required />
+          </div>
+          <button type="submit">{status}</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
-
-        
-        </div>
-      </SlidingBox>
-      );
+    </SlidingBox>
+  );
 };
 
 export default ContactForm;
+
+// 1. credentials from emailJS-
+
+// const userId = "user_ziX5oSLNJRahUxs9dz2xC";
+// const serviceId = "service_whc7i1l";
+// const templateId = "template_xhux42i";
+
+// 2. submit function which goes with Submit Button.Button
+// const submitInfo = () => {
+
+//   let contactInfo = {
+//     fullName: inputs.name,
+//     emailAddress: inputs.email,
+//     message: inputs.message
+//   };
+
+//   emailjs.send(serviceId, templateId, contactInfo, userId);
