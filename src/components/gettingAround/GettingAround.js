bfa687ba5288d12944c3ea0694b9e668f33827bc
First Last <samoviskik@gmail.com>
2021-10-11 15:05:38 -0400

    added walking tour info

--- src/components/gettingAround/GettingAround.js
+++ src/components/gettingAround/GettingAround.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import SlidingBox from "../slidingBox/SlidingBox";
+
 import map from "../../assets/BridgeMain.jpg";
 import styles from "./GettingAround.module.css";
 
@@ -15,7 +17,37 @@ const GettingAround = (props) => {
 
   }, []);
 
-  return <img src={map} className={`${styles.map} ${visible ? styles.open : ''}`}></img>;
+  return (
+    <SlidingBox>
+       <div className={styles.innerContentContainer}>
+    <div className={styles.statementDiv}>
+    <p>
+      TOUR DETAILS</p>
+      <p>This walk is approximately 1.85 miles and takes about 1 hour to complete.</p>
+      <hr/>
+      <p>SCULPTURES</p> 
+
+      <p>	1/12	Bridge Medallions <br/>
+	2	Riverside Mallards <br/>
+	3	High Water Mark 1913 Flood <br/>
+	4	Family of Man <br/>
+	5	Tulips for Theresa Bench <br/>
+	6	Embrace <br/>
+	7	When it rains it pours <br/>
+	8	Beetle Bench <br/>
+	9	Two Halves of a Whole <br/>
+	10	Sherry Corbett Memorial Sculpture <br/>
+	11	Space Tower</p> 
+      
+      </div>
+      </div>
+
+      <div className={styles.mapImage}>
+  <img src={map} className={`${styles.map} ${visible ? styles.open : ''}`}></img>
+  </div>
+
+  </SlidingBox>
+  );
 };
 
 export default GettingAround;
--- src/components/gettingAround2/GettingAround2.js
+++ src/components/gettingAround2/GettingAround2.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 
+import SlidingBox from "../slidingBox/SlidingBox";
+
+
 import map from "../../assets/Downtown.jpg";
 import styles from "./GettingAround2.module.css";
 
@@ -15,7 +18,58 @@ const GettingAround2 = (props) => {
 
   }, []);
 
-  return <img src={map} className={`${styles.map} ${visible ? styles.open : ''}`}></img>;
+  return (
+    <SlidingBox>
+       <div className={styles.innerContentContainer}>
+    <div className={styles.statementDiv}>
+    <p>
+      TOUR DETAILS</p>
+      <p>This walk is approximately 2 miles and takes about 1 hour to complete.</p>
+      <hr/>
+      <p>SCULPTURES</p> 
+
+      <p>	1	Spring
+	2	Shared Vision<br/>
+	3	First Ride<br/>
+	4	Firefighter Memorial<br/>
+	5	Pioneer Family<br/>
+	6	Victory (on top of Monument Building)<br/>
+	7	Legacy of Literature<br/>
+	8	Kitty Bench<br/>
+	9	Double Crazy Swirl<br/>
+	10	Lentil<br/>
+	11	Snapshot<br/>
+	12	Urban Renewal<br/>
+	13	Park Place<br/>
+	14	Resurgence<br/>
+	15	4 People<br/>
+	16	Guitar<br/>
+	17	Marcum Heart in the Park<br/>
+	18	Double Dancer<br/>
+	19	Tree Bench<br/>
+	20	The Maestro<br/>
+	21	Bird House<br/>
+	22	Republic Car<br/>
+	23	A Forest Treasure<br/>
+	24	Here We Go<br/>
+	25	Keeper of the Garden<br/>
+	26	Hebe<br/>
+	27	Hamilton Gateway<br/>
+	28	The American Cape<br/>
+	29	Conversation in Six Parts<br/>
+	30	Collective Impact<br/>
+	31	Come Unto Me<br/>
+	32	Hanna</p> 
+      
+      </div>
+      </div>
+
+      <div className={styles.mapImage}>
+  <img src={map} className={`${styles.map} ${visible ? styles.open : ''}`}></img>
+  </div>
+
+  </SlidingBox>
+  );
 };
 
 export default GettingAround2;
